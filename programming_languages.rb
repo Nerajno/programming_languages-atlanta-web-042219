ae28136f1f62c4cccc9d24dcdb5fcae11c9b48a1
active-object-6703 <nerajno@gmail.com>
2019-04-07 22:56:15 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,8 +2,9 @@ def reformat_languages(languages)
   # your code here
   hash = {}
   languages.each do |lang_types, langs|
-      hash[lang_types]=
-      puts langs
+    langs.each do |key, val|
+      puts key,val,{:style =>[lang_types]}
+
+    end
   end
-  puts hash
 end
