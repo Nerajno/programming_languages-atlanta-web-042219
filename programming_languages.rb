b87735b69492b2c3e8e9b65a6ee172cc91e1ca8c
active-object-6703 <nerajno@gmail.com>
2019-04-10 16:40:50 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,11 +1,19 @@
+require 'pry'
+
 def reformat_languages(languages)
   # your code here
   hash = {}
-  languages.each do |lang_types, langs|
+  languages.each do |lang_styles, langs|
     langs.each do |key, val|
       # puts key,val,:style =>[lang_types]
-      #closer still a bit lost
-    hash[key]=val
+      #language = langs.key, the language type = langs.values,
+      #styles = lang_styles, for each language,
+      #use the language as the key & {the language type
+      #as key/val pair 1 and styles as key/val pair 2
+      #the style val is an array which is appended to/
+      #pushed if the value is not found.
+      binding.pry
+      hash[langs.key]=langs.valves
     end
   end
   puts hash
